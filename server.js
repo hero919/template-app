424c52a9ce98e045223317c246eecb209afa49f0
Zeqing Zhang <terry.zhang@cspi.com>
2020-08-15 14:34:35 -0400

    Set up heroku configuration

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 var express = require("express");
-var mysql = require("mysql");
+// var mysql = require("mysql");
 
 var app = express();
 
-app.get("/", function(req, res) {
-  const connection = mysql.createConnection({
-      host: "",
-      user: "",
-      password: "",
-      database: ""
-  });
+// app.get("/", function(req, res) {
+//   const connection = mysql.createConnection({
+//       host: "",
+//       user: "",
+//       password: "",
+//       database: ""
+//   });
+//
+//   connection.query("", (err, rows, fields) => {
+//      res.json(rows);
+//   })
+//
+// })
 
-  connection.query("", (err, rows, fields) => {
-     res.json(rows);
-  })
-
-})
-// process.env.PORT is naming convenstion for port info
-// in heroku
-const port = process.env.PORT || 3000
 // Looking for current directory with project name found in package.json
 app.use(express.static(__dirname, "/dist/template-app"));
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname + "/dist/template-app/index.html"));
+});
 
-
-app.listen(port);
+// process.env.PORT is naming convention for port info
+// in heroku
+app.listen(process.env.PORT || 8080);
